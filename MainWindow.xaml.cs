95b6bed5d6ed45249c0c6cfcf1912a892e03ea83
VOUSHIKHA BALKISSOON <30077230@tafe.wa.edu.au>
2024-10-08 14:32:48 +0800

    implement font style methods

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+
 
 namespace kanban
 {
@@ -20,7 +14,29 @@ namespace kanban
         {
             InitializeComponent();
         }
+
+
+
+        
+            
+
        
-      
+        #region
+
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // tb1.FontFamily = new FontFamily(dlg.Font.FontFamily.Name);
+                tb1.FontSize = fontDialog.Font.Size * 96.0 / 72.0;
+                tb1.FontWeight = fontDialog.Font.Bold ? FontWeights.Bold : FontWeights.Normal;
+                tb1.FontStyle = fontDialog.Font.Italic ? FontStyles.Italic : FontStyles.Normal;
+            }
+        }
+
+
     }
 }
